3eae97b98009197124d2272a8ed6fa8b6578de6b
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2021-11-22 19:47:41 +0300

    Initialize

---
+++ dairemodul/circle.js
@@ -0,0 +1,14 @@
+function circleArea(r) {
+
+    return Math.PI * Math.pow(r,2);
+
+}
+
+function circleCircumference(r) {
+    return 2 * Math.PI * 2;
+}
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
---
+++ dairemodul/index.js
@@ -0,0 +1,6 @@
+const { circleArea, circleCircumference} = require("./circle.js");
+
+let alan = circleArea(5);
+let cevre = circleCircumference(5);
+
+console.log("Dairenin alanı: " + alan + ", Çevresi: " + " " + cevre + " birimdir");
