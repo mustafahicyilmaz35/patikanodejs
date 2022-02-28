f041ae18e8b593837bfd57888ffee6e897ef759f
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2022-02-28 23:49:29 +0300

    cleanblog ejs odevi

--- CleanBlog/CleanBlog/app.js
+++ CleanBlog/CleanBlog/app.js
@@ -1,14 +1,35 @@
 const express = require('express');
-
+const ejs = require('ejs');
 const app = express();
 
+
+
+app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  // const blog = {
+  //   id: 1,
+  //   title: 'Blog title',
+  //   description: 'Blog description',
+  // };
+  // res.send(blog);
+  res.render('index');
+});
+
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+
+app.get('/about', (req, res) => {
+ 
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
